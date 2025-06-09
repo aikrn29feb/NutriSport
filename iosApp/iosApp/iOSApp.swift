83e0e181e2923bc1a125f2948d115751ff54e537
Kiran <kiranchavan.dev@gmail.com>
2025-06-09 16:46:03 +0530

    removed unnecessary code

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,8 +1,6 @@
 import SwiftUI
 import GoogleSignIn
 import Firebase
-import FirebaseCore
-import FirebaseAuth
 
 @main
 struct iOSApp: App {
@@ -26,23 +24,5 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         FirebaseApp.configure()
         return true
     }
-
-    func application(
-      _ app: UIApplication,
-      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-      var handled: Bool
-
-      handled = GIDSignIn.sharedInstance.handle(url)
-      if handled {
-        return true
-      }
-
-      // Handle other custom URL types.
-
-      // If not handled by this app, return false.
-      return false
-    }
-
 
 }
